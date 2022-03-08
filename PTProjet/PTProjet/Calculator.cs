83bfe818503c8711e71622821980696e2cffba00
cleme@LAPTOP-7RRE6SIU.(none) <clement.moreau92@gmail.com>
2022-03-08 21:31:55 +0100

    test

---
+++ PTProjet/PTProjet/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace PTProjet
+{
+    public class Calculator
+    {
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Substract(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Multiply(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Divide(float x, float y)
+        {
+            return x / y;
+        }
+    };
+}
