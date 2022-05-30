67ffb2c1f0fd54defa4f5507b7168dad4b49f44d
Jean Augier <904121@edu.p.lodz.pl>
2022-05-30 19:38:48 +0200

    Random string generation added

---
+++ PTProjet/TestProjectTask2/PresentationTest/PresentationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestProjectTask2.PresentationTest
+{
+    /*[TestClass]
+    public class MainWindowViewModelUnitTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            //MainWindowViewModel window = new MainWindowViewModel();
+            //Assert.IsNotNull(window);
+        }
+    }*/
+    
+}
